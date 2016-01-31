f9f50a0150f7bcc3d36fb57973f8461c99060e8b
Matt Rayan <mattrayan@users.noreply.github.com>
2016-01-31 01:41:22 -0500

    Update directory hierarchy to include "State"

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ angular.module('musicdb', ['ui.router', 'flux', 'angularSoundManager'])
    $stateProvider
     .state('discovery', {
         url: '/',
-        templateUrl: './templates/home.html'
+        templateUrl: './states/discovery/discovery.html'
     });
 
 });
