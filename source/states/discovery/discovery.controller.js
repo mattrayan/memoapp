e7a1e775b867e7ea3850273a99db5ee4eb5f41b8
Matt Rayan <mattrayan@users.noreply.github.com>
2016-02-09 18:12:57 -0500

    Setup welcome state

--- source/states/discovery/discovery.controller.js
+++ source/states/discovery/discovery.controller.js
@@ -1,11 +1,14 @@
-angular.module('discovery', [])
+angular.module('discovery', ['ngAnimate'])
 .controller('discoveryCtrl', ['$scope', 'spotifyAPI', function($scope, spotifyAPI) {
+    var self = this;
 
     this.query = {};
+    this.welcome = true;
 
     this.search = function() {
         spotifyAPI.getArtists(this.query.request).then(function(data) {
             console.log(data);
+            self.welcome = false;
         });
     };
 
